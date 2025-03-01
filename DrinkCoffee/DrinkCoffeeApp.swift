c1180fa5201602b675826616538c28500a3fc3a7
janerkeussen <zhanerke.usen@gmail.com>
2025-03-01 15:39:29 -0800

    Initial project setuo

---
+++ DrinkCoffee/DrinkCoffeeApp.swift
@@ -0,0 +1,27 @@
+//
+//  DrinkCoffeeApp.swift
+//  DrinkCoffee
+//
+//  Created by Zhanerke Ussen on 01/03/2025.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct DrinkCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
