e6eab0703cbba097aede0eb6dd1ef919a66b98e0
janerkeussen <zhanerke.usen@gmail.com>
2025-03-15 16:09:04 -0700

    Show drinks in home page

--- DrinkCoffee/HomeView.swift
+++ DrinkCoffee/HomeView.swift
@@ -8,29 +8,38 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    var categories: [String : [Drink]] {
+        .init(grouping: drinkData) { $0.category.rawValue }
+    }
+    
     var body: some View {
         NavigationStack {
-            Text("Hello, Alex")
-                .navigationTitle("DrinkCoffee")
-                .toolbar {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button {
-                            print("basket")
-                        } label: {
-                            Image("basket")
-                                .renderingMode(.template)
-                        }
-                    }
-                    
-                    ToolbarItem(placement: .topBarLeading) {
-                        Button {
-                            print("Log out")
-                        } label: {
-                            Text("Log out")
-                                .fontWeight(.semibold)
-                        }
+            List(categories.keys.sorted(), id: \String.self) { key in
+                DrinkRow(categoryName: key, drinks: categories[key] ?? [])
+                    .frame(height: 320)
+                    .padding([.top, .bottom])
+            }
+            .navigationTitle("DrinkCoffee")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        print("basket")
+                    } label: {
+                        Image("basket")
+                            .renderingMode(.template)
                     }
                 }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        print("Log out")
+                    } label: {
+                        Text("Log out")
+                            .fontWeight(.semibold)
+                    }
+                }
+            }
         }
     }
 }
